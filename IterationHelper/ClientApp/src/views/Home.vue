b9357dea98cdb195092a304c373f6372f15e107e
wpe19d <WesEaston>
2021-03-16 12:13:14 -0500

    initial push

---
+++ IterationHelper/ClientApp/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-layout row wrap>
+    <v-card style="width: 100%;" flat>
+      <v-container fill-height class="home">
+          <home-content/>
+      </v-container>
+    </v-card>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+import HomeContent from '../components/HomeContent.vue';
+
+  @Component({
+    components: {
+      HomeContent
+    }
+  })
+
+export default class Home extends Vue {
+
+}
+</script>
---
+++ IterationHelper/Startup.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using VueCliMiddleware;
+
+namespace IterationHelper
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "IterationHelperApi v1"));
+            }
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = env.IsDevelopment() ? "ClientApp" : "dist";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseVueCli(npmScript: "serve");
+                }
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "IterationHelperApi", Version = "v1"});
+
+            });
+
+
+        }
+    }
+}
